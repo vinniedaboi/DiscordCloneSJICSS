fc5278b9c8777e046f99a118b758cd6096860647
vinniedaboi <kidosol5224@gmail.com>
2024-07-05 20:49:23 +0800

    Finish up Project

--- lib/initial-profile.ts
+++ lib/initial-profile.ts
@@ -1,32 +1,37 @@
 import { currentUser, redirectToSignIn } from "@clerk/nextjs";
-
 import { db } from "@/lib/db";
 
 export const initialProfile = async () => {
-  const user = await currentUser();
+  try {
+    const user = await currentUser();
 
-  if (!user) {
-    return redirectToSignIn();
+    if (!user) {
+      return redirectToSignIn();
+    }
+
+    const profile = await db.profile.findUnique({
+      where: {
+        userId: user.id,
+      },
+    });
+
+    if (profile) {
+      return profile;
+    }
+
+    const newProfile = await db.profile.create({
+      data: {
+        userId: user.id,
+        name: `${user.firstName} ${user.lastName}`,
+        imageUrl: user.imageUrl,
+        email: user.emailAddresses[0].emailAddress,
+      },
+    });
+
+    return newProfile;
+
+  } catch (error) {
+    console.error("Error fetching or creating profile:", error);
+    throw new Error("An error occurred while fetching or creating the profile.");
   }
-
-  const profile = await db.profile.findUnique({
-    where: {
-      userId: user.id,
-    },
-  });
-
-  if (profile) {
-    return profile;
-  }
-
-  const newProfile = await db.profile.create({
-    data: {
-      userId: user.id,
-      name: `${user.firstName} ${user.lastName}`,
-      imageUrl: user.imageUrl,
-      email: user.emailAddresses[0].emailAddress,
-    },
-  });
-
-  return newProfile;
 };
